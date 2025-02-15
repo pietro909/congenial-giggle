5049bd4992cf40879122df53b9092b233a1132e6
Louis Singer <41042567+louisinger@users.noreply.github.com>
2025-02-15 22:05:57 +0100

    export ArkAddress and VtxoTapscript (#17)

--- packages/ts-sdk/src/index.ts
+++ packages/ts-sdk/src/index.ts
@@ -1,7 +1,9 @@
 import { InMemoryKey } from "./core/identity";
+import { ArkAddress } from "./core/address";
+import { VtxoTapscript } from "./core/tapscript";
 import { Wallet } from "./core/wallet";
 import type { WalletConfig } from "./types/wallet";
 import { ESPLORA_URL } from "./providers/esplora";
 
 export type { WalletConfig };
-export { Wallet, InMemoryKey, ESPLORA_URL };
+export { Wallet, InMemoryKey, ESPLORA_URL, ArkAddress, VtxoTapscript };
