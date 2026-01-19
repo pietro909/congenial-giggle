696f7cf6aa006bde27860fc0ca165663e1ff36c8
João Bordalo <bordalix@users.noreply.github.com>
2026-01-19 21:28:26 +0000

    Fix integration test (#53)
    * fix deprecated usage of it()
    
    * cancel invoice instead of stopping fulmine to make VHTLC fail

--- packages/boltz-swap/test/e2e/integration.test.ts
+++ packages/boltz-swap/test/e2e/integration.test.ts
@@ -5,45 +5,46 @@ import { exec } from "child_process";
 import { promisify } from "util";
 import { expect } from "vitest";
 
-it(
-    "should recover swept VHTLCs",
-    async () => {
-        const { arkadeLightning, wallet } = await createTestWallet();
+it("should recover swept VHTLCs", { timeout: 120_000 }, async () => {
+    const { arkadeLightning, wallet } = await createTestWallet();
 
-        const { stdout } = await promisify(exec)(
-            `docker exec lnd lncli --network=regtest addinvoice --amt 1000`
-        );
-        const output = stdout.trim();
-        expect(output).toBeDefined();
-        expect(output).toBeTruthy();
-        const outputJSON = JSON.parse(output);
-        expect("payment_request" in outputJSON).toBeTruthy();
-        const invoice = outputJSON.payment_request;
+    const { stdout } = await promisify(exec)(
+        `docker exec lnd lncli --network=regtest addinvoice --amt 1000`
+    );
+    const output = stdout.trim();
+    expect(output).toBeDefined();
+    expect(output).toBeTruthy();
+    const outputJSON = JSON.parse(output);
+    expect("payment_request" in outputJSON).toBeTruthy();
+    const invoice = outputJSON.payment_request;
 
-        const swap = await arkadeLightning.createSubmarineSwap({ invoice });
-        // fund the vhtlc after fulmine is down so it can be swept
-        exec(`docker compose -f test.docker-compose.yml stop boltz-fulmine`);
-        exec(
-            `docker exec -t arkd ark send --to ${swap.response.address} --amount ${swap.response.expectedAmount} --password secret`
-        );
+    // cancel invoice to make the swap fail
+    expect("r_hash" in outputJSON).toBeTruthy();
+    const hash = outputJSON.r_hash;
+    exec(`docker exec lnd lncli --network=regtest cancelinvoice ${hash}`);
 
-        // generate block to expire the vhtlc
-        exec(
-            `nigiri rpc generatetoaddress 21 bcrt1qlfc0juf4csvvfkvxhzygqgh523sc9ppvq4yz3v`
-        );
+    const swap = await arkadeLightning.createSubmarineSwap({ invoice });
 
-        // sleep 30s to let arkd sweep the vhtlc
-        await new Promise((resolve) => setTimeout(resolve, 30_000));
-        // try to refund (with boltz down and vhtlc swept)
-        await arkadeLightning.refundVHTLC(swap);
+    // fund the vhtlc after is canceled so it can be swept
+    exec(
+        `docker exec -t arkd ark send --to ${swap.response.address} --amount ${swap.response.expectedAmount} --password secret`
+    );
 
-        await new Promise((resolve) => setTimeout(resolve, 1500));
+    // generate block to expire the vhtlc
+    exec(
+        `nigiri rpc generatetoaddress 21 bcrt1qlfc0juf4csvvfkvxhzygqgh523sc9ppvq4yz3v`
+    );
 
-        const balance = await wallet.getBalance();
-        expect(balance.available).toBe(swap.response.expectedAmount);
-    },
-    { timeout: 120_000 }
-);
+    // sleep 30s to let arkd sweep the vhtlc
+    await new Promise((resolve) => setTimeout(resolve, 30_000));
+    // try to refund (with boltz down and vhtlc swept)
+    await arkadeLightning.refundVHTLC(swap);
+
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
+    const balance = await wallet.getBalance();
+    expect(balance.available).toBe(swap.response.expectedAmount);
+});
 
 export interface TestArkWallet {
     wallet: Wallet;
